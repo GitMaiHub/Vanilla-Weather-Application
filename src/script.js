dcf8fec3af2164687cb3fd3b02804906220c6ba1
GitMaiHub <h.maibcarlton.i@gmail.com>
2022-08-09 17:17:25 -0600

    Updated date in script.js and capitalization in style.css.
    script.js - added formatDate function to pull date and time using new Date(timestamp).
    
    style.css - capitalize only the first letter of words.

--- src/script.js
+++ src/script.js
@@ -2,13 +2,27 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
   let minutes = date.getMinutes();
-  let day = date.getDay();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
 // Initialize and invoke displayTemperature function that pulls data from OpenWeatherMap
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature"); // selecting temperature that alters
   temperatureElement.innerHTML = Math.round(response.data.main.temp); // pull the data of the city entered from OpenWeatherMap
   let cityElement = document.querySelector("#city");
