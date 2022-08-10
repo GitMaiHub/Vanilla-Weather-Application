4274525e0ec642fdc43e68e694f7f0651995fd4a
GitMaiHub <h.maibcarlton.i@gmail.com>
2022-08-10 16:18:14 -0600

    Updated temperature conversion in index.html and script.js.
    index.html - added a link to celsius degree.
    
    script.js - added functions displayFahrenheitTemperature(event) and displayCelsiusTemperature(event). made C and F links appear and disappear when C or F gets clicked accordingly.

--- src/script.js
+++ src/script.js
@@ -3,11 +3,11 @@ function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
-    hours = `0${hours}`;
+    hours = `0${hours}`; // add 0 before hours (eg. 07:20 instead of 7:20)
   }
   let minutes = date.getMinutes();
   if (minutes < 10) {
-    minutes = `0${minutes}`;
+    minutes = `0${minutes}`; // add 0 before minutes (eg. 17:07 instead of 17:7)
   }
   let days = [
     "Sunday",
@@ -17,14 +17,15 @@ function formatDate(timestamp) {
     "Thursday",
     "Friday",
     "Saturday",
-  ];
+  ]; // must define all the "days" from Sunday before defining "day" in order not to get a number instead
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
-// Initialize and invoke displayTemperature function that pulls data from OpenWeatherMap
+// Create displayTemperature function that pulls data from OpenWeatherMap
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature"); // selecting temperature that alters
-  temperatureElement.innerHTML = Math.round(response.data.main.temp); // pull the data of the city entered from OpenWeatherMap
+  celsiusTemperature = response.data.main.temp;
+  temperatureElement.innerHTML = Math.round(celsiusTemperature); // pull the data of the city entered from OpenWeatherMap
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
   let descriptionElement = document.querySelector("#description");
@@ -39,7 +40,7 @@ function displayTemperature(response) {
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
+  ); // pull weather icon from OpenWeatherMap, using icon url and interpolate the data set in "number+d"
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 // Create search function to receive a city, invoking AJAX call
@@ -54,7 +55,33 @@ function handleSubmit(event) {
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
 }
-search("Seattle"); // display default city
+// Takes care of temperature conversion from Celsius to Fahrenheit
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.remove("active"); // when F degree is shown, C link does not appear active
+  fahrenheitLink.classList.add("active"); // when F degree is shown, F link appears active
+  let fahrenheitTemperature = Math.round(celsiusTemperature * 9) / 5 + 32;
+  let temperatureElement = document.querySelector("#temperature"); // recalling temperatureElement in order to convert celsius to fahrenheit
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active"); // when C degree is shown, C link appears active
+  fahrenheitLink.classList.remove("active"); // when C degree is shown, F link does not appear active
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature); // celsius temperature is the same as temperatureElement
+}
+
+// form, fahrenheitLink, celsiusLink are global variables which are accessible from inside functions above
+let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("Seattle"); // display default city
